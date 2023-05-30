df7d0d80108cb4679062b7e0eae9a1b686644fb0
cwpark <didtks102@naver.com>
2023-05-31 06:40:41 +0900

    hotfix1 commit

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
+require("dotenv/config");
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
+const { Client, IntentsBitField } = require("discord.js");
+const { Configuration, OpenAIApi } = require("openai");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
 client.commands = new Collection();
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+  if (message.channel.id !== process.env.CHANNEL_ID) return;
+  if (message.content.startsWith("!")) return;
 
 for (const folder of commandFolders) {
 	const commandsPath = path.join(foldersPath, folder);
@@ -36,5 +50,26 @@ for (const file of eventFiles) {
 		client.on(event.name, (...args) => event.execute(...args));
 	}
 }
+
+prevMessages.forEach((msg) => {
+    if (message.content.startsWith("!")) return;
+    if (msg.author.id !== client.user.id && message.author.bot) return;
+    if (msg.author.id !== message.author.id) return;
+
+    conversationLog.push({
+      role: "user",
+      content: message.content,
+    });
+  });
+
+  const result = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: conversationLog,
+  });
+
+  message.reply(result.data.choices[0].message);
+});
+
+client.login(process.env.TOKEN);
 
 client.login(token);
